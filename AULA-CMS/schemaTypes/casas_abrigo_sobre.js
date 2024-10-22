1aee40d5638470210cc3803290a8cd05c2801300
Amanda <freitasamandalais@gmail.com>
2024-10-21 21:30:05 -0300

    feature: Casas Abrigo (Sobre)

---
+++ AULA-CMS/schemaTypes/casas_abrigo_sobre.js
@@ -0,0 +1,22 @@
+import { defineField, defineType } from "sanity";
+
+export const sobreCasasAbrigoType = defineType({
+    name: 'casasabrigo_sobre',
+    title: 'Casas Abrigo (Sobre)',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'heading_1',
+            title: 'Tópico',
+            type: 'string',
+        }),
+        defineField({
+            name: 'body',
+            title: 'Texto',
+            type: 'array',
+            of: [
+                { type: 'string' }
+            ]
+        }),
+    ]
+});
--- AULA-CMS/schemaTypes/index.js
+++ AULA-CMS/schemaTypes/index.js
@@ -1,15 +1,11 @@
 import { adolfoType } from './adolfo';
 import { amandaType } from "./amanda"
 import { carlosType } from './carlos'
+import { sobreCasasAbrigoType } from './casas_abrigo_sobre';
 import { joaoType } from "./joao"
 import { luanType } from './luan';
 import { productType } from './lucas';
 
 export const schemaTypes = [
-  joaoType,
-  amandaType,
-  carlosType,
-  productType,
-  adolfoType,
-  luanType
+  sobreCasasAbrigoType
 ];
